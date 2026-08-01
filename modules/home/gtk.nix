f815f39615e282275590d8b83966b09acef21fa5
liangzid <2273067585@qq.com>
2026-08-01 14:21:56 +0800

    enable fcitx5 wayland frontend and rime, move fcitx5 config declarative

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -1,6 +1,15 @@
 { config, pkgs, ... }: {
   gtk = {
     enable = true;
+    # waylandFrontend=true 后 GTK_IM_MODULE 不再全局设置；X11/XWayland
+    # 的 GTK 应用通过 settings.ini 指定 fcitx 模块，Wayland 原生 GTK
+    # 应用则走 text-input-v3。
+    gtk3.extraConfig = {
+      "gtk-im-module" = "fcitx";
+    };
+    gtk4.extraConfig = {
+      "gtk-im-module" = "fcitx";
+    };
     theme = {
       name = "Juno";
       package = pkgs.juno-theme;
